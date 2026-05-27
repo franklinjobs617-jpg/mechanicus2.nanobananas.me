d88d3a2754243e51939d3a1202c571fb2047ba43
zhangtao <git.vistaflyer.com>
2026-05-27 12:08:25 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -73,6 +73,19 @@ export default function RootLayout({
           }}
         ></Script>
 
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DJXK1Z37XG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DJXK1Z37XG');
+          `}
+        </Script>
+
         <ResourcePreload />
         <script
           type="application/ld+json"
